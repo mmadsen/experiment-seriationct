c46ce0b76f4cbcc1ec840d127fa9aec2f123f780
Mark Madsen <mark@madsenlab.org>
2015-03-16 16:32:59 -0700

    New run of the CA seriations, removing space in PNG file names. Script for CA analysis now takes a command line parameter for the directory you want to run it on, for example:  rscript CA_Seriation_By_Directory.R seriationct-1/resample-1

--- ca-seriations/CA_Seriation_By_Directory.R
+++ ca-seriations/CA_Seriation_By_Directory.R
@@ -1,20 +1,36 @@
-install.packages("devtools", dependencies=TRUE)
 library(devtools)
 library(ca)
 library(seriation)
 library(plotrix)
 library(mmadsenr)
+library(stringr)
+
+# command line argument processing
+args <- commandArgs(trailingOnly = TRUE)
+
+directory_to_process <- args[1]
+
 
 filenameRoot <- function(x){
-  substr(x, 0, nchar(x)-4)
+  tmp <- substr(x, 0, nchar(x)-4)
+  str_trim(tmp)
 }
 
 file_root <- function(x) {
   substr(x,nchar(x)-46,nchar(x)-4)
 }
-setwd("/Users/clipo/PycharmProjects/seriationct/ca/resample-1/")
-file_list <- list_files_for_data_path(directory = "./")
+
+
+base_dir <- getwd()
+full_dir_to_process <- paste(base_dir, directory_to_process, sep="/") 
+
+setwd(full_dir_to_process)
+cat(sprintf("directory to process: %s\n", full_dir_to_process))
+
+file_list <- list_files_for_data_path(basedir = base_dir, directory = directory_to_process)
  
+cat(sprintf("file list: %s\n", file_list))
+
 for (file in file_list){
   # read data from choosen file
   mydata <- read.table(file, row.names=1, header=T)
@@ -30,7 +46,7 @@ for (file in file_list){
   col.c<-res.ca$colcoord[,2]
   #sort the table according to the coord of the selected CA dimension and plot seriation chart (Bertin plot)
   print(sorted.table<-pfg[order(row.c), order(col.c)]) #sort the table
-  graph_name <- paste(filenameRoot(file),".png")
+  graph_name <- paste(filenameRoot(file),".png",sep="")
   png(graph_name)
   dev.copy(png,graph_name)
   battleship.plot(sorted.table, main=file_root(graph_name))
