90a3158ac07db625046698e8a00184b8183e978a
Mark Madsen <mark@madsenlab.org>
2015-03-25 12:52:51 -0700

    seriationct-6 simulation results and sampled IDSS input files.  Ready to start seriations on the EC2 server.

--- experiments/seriationct-6/bin/simulation-postprocess.sh
+++ experiments/seriationct-6/bin/simulation-postprocess.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 seriationct-export-data.py --experiment seriationct-6 --outputdirectory exported-data
 seriationct-assemblage-duration-export.py --experiment seriationct-6 --outputdirectory temporal
-seriationct-sample-assemblages-for-seriation.py --inputdirectory exported-data --outputdirectory sampled-data --samplefraction 0.3 --debug 0
+seriationct-sample-assemblages-for-seriation.py --inputdirectory exported-data --outputdirectory sampled-data --samplefraction 0.2 --debug 0
 
--- template/bin/build-simulations.sh
+++ template/bin/build-simulations.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-seriationct-runbuilder.py --experiment REPLACEME --expconfig seriationct-priors-1rep.json --parallelism 1 --numsims 4 --networkmodels networks --debug 0 --jobdirectory jobs
+seriationct-runbuilder.py --experiment REPLACEME --expconfig seriationct-priors.json --parallelism 1 --numsims 4 --networkmodels networks --debug 0 --jobdirectory jobs
